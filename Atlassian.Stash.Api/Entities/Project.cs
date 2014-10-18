9518ac3ca611466a455e7ecd7a094905e5db69eb
jlouros <jlouros@pointroll.com>
2014-10-17 21:55:26 -0400

    inital code commit

---
+++ Atlassian.Stash.Api/Entities/Project.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Atlassian.Stash.Api.Entities
+{
+    public class Project
+    {
+        public string Key { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool @Public { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ Atlassian.Stash.Api/Entities/Repository.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Atlassian.Stash.Api.Entities
+{
+    public class Repository
+    {
+        public string Slug { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ScmId { get; set; }
+        public string State { get; set; }
+        public string StatusMessage { get; set; }
+        public bool Forkable { get; set; }
+        public bool @Public { get; set; }
+        public string CloneUrl { get; set; }
+    }
+}
