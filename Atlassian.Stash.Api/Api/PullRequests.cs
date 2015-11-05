c5c31197f28856dd6e200888b947ffdcfa9fb21c
David Rose <David.Rose@d2l.com>
2015-11-04 20:01:18 -0500

    Added Get functions to get pull requests Changed Pull Requests state option to an enum Refactored UrlFormatter

--- Atlassian.Stash.Api/Api/PullRequests.cs
+++ Atlassian.Stash.Api/Api/PullRequests.cs
@@ -7,26 +7,77 @@ using Atlassian.Stash.Api.Entities;
 using Atlassian.Stash.Api.Helpers;
 using Atlassian.Stash.Api.Workers;
 
-namespace Atlassian.Stash.Api.Api
-{
-    public class PullRequests
-    {
-        private const string PULL_REQUEST = "/rest/api/1.0/projects/{0}/repos/{1}/pull-requests";
-        private HttpCommunicationWorker _httpWorker;
+namespace Atlassian.Stash.Api.Api {
+	public class PullRequests {
 
-        internal PullRequests(HttpCommunicationWorker httpWorker)
-        {
-            _httpWorker = httpWorker;
-        }
+		public enum Order {
+			OLDEST,
+			NEWEST
+		}
 
-        public async Task<PullRequest> Create(string projectKey, string repositorySlug,
-            PullRequest pullRequest)
-        {
-            string requestUrl = UrlBuilder.FormatRestApiUrl(PULL_REQUEST, null, projectKey, repositorySlug);
+		public enum Direction {
+			INCOMING,
+			OUTGOING
+		}
 
-            PullRequest pr = await _httpWorker.PostAsync(requestUrl, pullRequest);
+		private const string PULL_REQUEST = "/rest/api/1.0/projects/{0}/repos/{1}/pull-requests";
+		private HttpCommunicationWorker _httpWorker;
 
-            return pr;
-        }
-    }
+		internal PullRequests(HttpCommunicationWorker httpWorker) {
+			_httpWorker = httpWorker;
+		}
+
+		public async Task<PullRequest> Create(string projectKey, string repositorySlug, PullRequest pullRequest) {
+			string requestUrl = UrlBuilder.FormatRestApiUrl(PULL_REQUEST, null, projectKey, repositorySlug);
+
+			PullRequest pr = await _httpWorker.PostAsync(requestUrl, pullRequest);
+
+			return pr;
+		}
+
+		public async Task<ResponseWrapper<PullRequest>> Get(string projectKey,
+			string repositorySlug,
+			RequestOptions options = null,
+			Direction direction = Direction.INCOMING,
+			PullRequestState state = PullRequestState.OPEN,
+			bool withAttributes = true,
+			bool withProperties = true)
+		{
+			string requestUrl = UrlBuilder.ToRestApiUrl(String.Format(PULL_REQUEST, projectKey, repositorySlug))
+			                              .WithOptions(options)
+			                              .WithQueryParam("direction", direction.ToString())
+			                              .WithQueryParam("state", state.ToString())
+			                              .WithQueryParam("withAttributes", withAttributes.ToString())
+			                              .WithQueryParam("withProperties", withProperties.ToString());
+
+			var pr = await _httpWorker.GetAsync<ResponseWrapper<PullRequest>>(requestUrl).ConfigureAwait(false);
+
+			return pr;
+
+		}
+
+		public async Task<ResponseWrapper<PullRequest>> Get(string projectKey,
+			string repositorySlug,
+			Order order,
+			RequestOptions options = null,
+			Direction direction = Direction.INCOMING,
+			PullRequestState state = PullRequestState.OPEN,
+			bool withAttributes = true,
+			bool withProperties = true)
+		{
+			string requestUrl = UrlBuilder.ToRestApiUrl(String.Format(PULL_REQUEST, projectKey, repositorySlug))
+			                              .WithOptions(options)
+			                              .WithQueryParam("direction", direction.ToString())
+			                              .WithQueryParam("state", state.ToString())
+			                              .WithQueryParam("withAttributes", withAttributes.ToString())
+			                              .WithQueryParam("withProperties", withProperties.ToString())
+			                              .WithQueryParam("order", order.ToString());
+
+			var pr = await _httpWorker.GetAsync<ResponseWrapper<PullRequest>>(requestUrl).ConfigureAwait(false);
+
+			return pr;
+
+		}
+
+	}
 }
