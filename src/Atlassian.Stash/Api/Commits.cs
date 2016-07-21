f3b14de39fbc2359d0879c687b5652900c00c621
Michael Russell <michael.russell@audatex.com>
2016-07-21 13:51:47 +0800

    Add methods to support retrieving ranges of commits.

--- src/Atlassian.Stash/Api/Commits.cs
+++ src/Atlassian.Stash/Api/Commits.cs
@@ -9,6 +9,8 @@ namespace Atlassian.Stash.Api
     {
         private const string MANY_COMMITS = "rest/api/1.0/projects/{0}/repos/{1}/commits";
         private const string ONE_COMMIT = "rest/api/1.0/projects/{0}/repos/{1}/commits/{2}";
+        private const string COMMITS_UNTIL = "rest/api/1.0/projects/{0}/repos/{1}/commits?until={2}";
+        private const string COMMITS_UNTIL_AND_SINCE = "rest/api/1.0/projects/{0}/repos/{1}/commits?until={2}&since={3}";
         private const string CHANGES_UNTIL = "rest/api/1.0/projects/{0}/repos/{1}/changes?until={2}";
         private const string CHANGES_UNTIL_AND_SINCE = "rest/api/1.0/projects/{0}/repos/{1}/changes?until={2}&since={3}";
 
@@ -50,6 +52,19 @@ namespace Atlassian.Stash.Api
                 requestUrl = UrlBuilder.FormatRestApiUrl(CHANGES_UNTIL_AND_SINCE, requestOptions, projectKey, repositorySlug, untilCommit, sinceCommit);
 
             Changes response = await _httpWorker.GetAsync<Changes>(requestUrl).ConfigureAwait(false);
+
+            return response;
+        }
+
+        public async Task<ResponseWrapper<Commit>> GetCommits(string projectKey, string repositorySlug, string untilCommit, string sinceCommit = null, RequestOptions requestOptions = null) {
+            string requestUrl = "";
+
+            if (string.IsNullOrWhiteSpace(sinceCommit))
+                requestUrl = UrlBuilder.FormatRestApiUrl(COMMITS_UNTIL, requestOptions, projectKey, repositorySlug, untilCommit);
+            else
+                requestUrl = UrlBuilder.FormatRestApiUrl(COMMITS_UNTIL_AND_SINCE, requestOptions, projectKey, repositorySlug, untilCommit, sinceCommit);
+
+            ResponseWrapper<Commit> response = await _httpWorker.GetAsync<ResponseWrapper<Commit>>(requestUrl).ConfigureAwait(false);
 
             return response;
         }
