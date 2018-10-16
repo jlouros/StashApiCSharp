6b187a4dba69d6ad1ad034f320f34dcf43a8d367
jrivam <jrivam@gmail.com>
2018-10-16 14:41:41 -0500

    Added Automerging, Branching model and Grant user permission to API (#59)
    * *Added LatestCommit property in Branch model
    *Added unnaproveOnUpdate on PullRequestSettings model
    *Added REBASE_AND_MERGE and REBASE_AND_FAST_FORWARD types to StrategyIdType enum
    *Added Automerging enabling in Branches api
    *Added Branching model configuration in Branches api
    *Added Grant user permission in Repositories api
    
    * remove blank space before closing bracket
    
    * include model items

---
+++ src/Atlassian.Stash/Entities/BranchModelConfigurationItem.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Atlassian.Stash.Entities
+{
+    public class BranchModelConfigurationItem
+    {
+        [JsonProperty("refId")]
+        public string RefId { get; set; }
+        [JsonProperty("useDefault")]
+        public bool UseDefault { get; set; }
+    }
+}
---
+++ src/Atlassian.Stash/Entities/BranchModelItem.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Atlassian.Stash.Entities
+{
+    public class BranchModelItem
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("displayName", NullValueHandling = NullValueHandling.Ignore)]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("prefix", NullValueHandling = NullValueHandling.Ignore)]
+        public string Prefix { get; set; }
+    }
+}
