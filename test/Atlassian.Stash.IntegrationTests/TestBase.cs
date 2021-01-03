9a3b24871778a331397a6bbc98af823416e8e55b
João Louros <jlouros@live.com>
2021-01-03 23:51:24 +0000

    Update dependencies (#72)
    * update dependencies
    
    * add GitHub action POC 1
    
    * make project compatible with msbuild 14
    
    * testing new project structure
    
    * remove old build scripts
    
    * unit test to .Net Core 3.1
    
    * update NuGet package strategy
    
    * focus on .Net Standard 2.0
    
    * test new version approach
    
    * try to fix build version
    
    * test package publish

--- test/Atlassian.Stash.IntegrationTests/TestBase.cs
+++ test/Atlassian.Stash.IntegrationTests/TestBase.cs
@@ -1,32 +1,45 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Atlassian.Stash.IntegrationTests
 {
     [TestClass]
     public abstract class TestBase
     {
+        public static readonly IConfiguration Configuration;
+
+        static TestBase()
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{environmentName}.json", true, true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
         // data required to run this tests, please check App.config and modify the values to mapping to your local stash instance
-        protected readonly string BASE_URL = ConfigurationManager.AppSettings.Get("base-url");
-        protected readonly string TEST_USERNAME = ConfigurationManager.AppSettings.Get("test-username");
-        protected readonly string TEST_PASSWORD = ConfigurationManager.AppSettings.Get("test-password");
-        protected readonly string OTHERTEST_USERNAME = ConfigurationManager.AppSettings.Get("othertest-username");
-        protected readonly string OTHERTEST_PASSWORD = ConfigurationManager.AppSettings.Get("othertest-password");
-        protected readonly string EXISTING_PROJECT = ConfigurationManager.AppSettings.Get("existing-project");
-        protected readonly string EXISTING_REPOSITORY = ConfigurationManager.AppSettings.Get("existing-repository");
-        protected readonly string EXISTING_FILE = ConfigurationManager.AppSettings.Get("existing-file");
-        protected readonly string EXISTING_FOLDER = ConfigurationManager.AppSettings.Get("existing-folder");
-        protected readonly string EXISTING_FILE_IN_SUBFOLDER = ConfigurationManager.AppSettings.Get("existing-file-in-subfolder");
-        protected readonly string EXISTING_FILE_IN_SUBFOLDER_WITH_SPACES = ConfigurationManager.AppSettings.Get("existing-file-in-subfolder-with-spaces");
-        protected readonly string EXISTING_COMMIT = ConfigurationManager.AppSettings.Get("existing-commit");
-        protected readonly string EXISTING_OLDER_COMMIT = ConfigurationManager.AppSettings.Get("existing-older-commit");
-        protected readonly string MASTER_BRANCH_REFERENCE = ConfigurationManager.AppSettings.Get("master-branch-reference");
-        protected readonly string DEVELOP_BRANCH_REFERENCE = ConfigurationManager.AppSettings.Get("develop-branch-reference");
-        protected readonly string MASTER_BRANCH_NAME = ConfigurationManager.AppSettings.Get("master-branch-name");
-        protected readonly string EXISTING_GROUP = ConfigurationManager.AppSettings.Get("existing-group");
-        protected readonly string EXISTING_HOOK = ConfigurationManager.AppSettings.Get("existing-hook");
-        protected readonly int EXISTING_NUMBER_OF_CHANGES = int.Parse(ConfigurationManager.AppSettings.Get("existing-number-of-changes"));
-        protected readonly string PERSONAL_ACCESS_TOKEN = ConfigurationManager.AppSettings.Get("personal-access-token");
+        protected readonly string BASE_URL = Configuration["testSettings:base-url"];
+        protected readonly string TEST_USERNAME = Configuration["testSettings:test-username"];
+        protected readonly string TEST_PASSWORD = Configuration["testSettings:test-password"];
+        protected readonly string OTHERTEST_USERNAME = Configuration["testSettings:othertest-username"];
+        protected readonly string OTHERTEST_PASSWORD = Configuration["testSettings:othertest-password"];
+        protected readonly string EXISTING_PROJECT = Configuration["testSettings:existing-project"];
+        protected readonly string EXISTING_REPOSITORY = Configuration["testSettings:existing-repository"];
+        protected readonly string EXISTING_FILE = Configuration["testSettings:existing-file"];
+        protected readonly string EXISTING_FOLDER = Configuration["testSettings:existing-folder"];
+        protected readonly string EXISTING_FILE_IN_SUBFOLDER = Configuration["testSettings:existing-file-in-subfolder"];
+        protected readonly string EXISTING_FILE_IN_SUBFOLDER_WITH_SPACES = Configuration["testSettings:existing-file-in-subfolder-with-spaces"];
+        protected readonly string EXISTING_COMMIT = Configuration["testSettings:existing-commit"];
+        protected readonly string EXISTING_OLDER_COMMIT = Configuration["testSettings:existing-older-commit"];
+        protected readonly string MASTER_BRANCH_REFERENCE = Configuration["testSettings:master-branch-reference"];
+        protected readonly string DEVELOP_BRANCH_REFERENCE = Configuration["testSettings:develop-branch-reference"];
+        protected readonly string MASTER_BRANCH_NAME = Configuration["testSettings:master-branch-name"];
+        protected readonly string EXISTING_GROUP = Configuration["testSettings:existing-group"];
+        protected readonly string EXISTING_HOOK = Configuration["testSettings:existing-hook"];
+        protected readonly int EXISTING_NUMBER_OF_CHANGES = int.Parse(Configuration["testSettings:existing-number-of-changes"]);
+        protected readonly string PERSONAL_ACCESS_TOKEN = Configuration["testSettings:personal-access-token"];
 
         protected StashClient stashClient;
 
